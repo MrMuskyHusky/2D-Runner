4e5afe1926797a4c683214c41374c4ba8406b0fb
Ethan McGimpsey <54158496+MrMuskyHusky@users.noreply.github.com>
2020-02-11 13:34:35 +1100

    Update #6

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,18 +1,37 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject gameOver;
+    public void NextScene()
     {
-        
+        // Goes straight to the next scene when a level is completed or a trigger is called.
+        Scene activeScene = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(activeScene.buildIndex + 1);
     }
-
-    // Update is called once per frame
-    void Update()
+    public void ResetLevel()
     {
-        
+        // Resets the current level of the scene when you hit a trigger or a ui button is pressed.
+        Scene activeScene = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(activeScene.buildIndex);
+    }
+    public void GameOver()
+    {
+        gameOver.SetActive(true);
+        Time.timeScale = 0;
+    }
+    public void MainMenu(int sceneIndex)
+    {
+        // Loads the scene that has been selected
+        SceneManager.LoadScene(0);
+    }
+    public void OnApplicationQuit()
+    {
+        // Quits the unity application
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
     }
 }
